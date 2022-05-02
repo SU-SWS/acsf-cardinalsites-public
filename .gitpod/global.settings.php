df79a87e159afa035d487e5f4e2173c15a7d2e33
pookmish <pookmish@stanford.edu>
2022-05-02 23:58:12 +0000

    global settings

---
+++ .gitpod/global.settings.php
@@ -0,0 +1,16 @@
+<?php
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+// Print errors on WSOD.
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+
+if (!getenv('STANFORD_ENCRYPT')) {
+  putenv("STANFORD_ENCRYPT=" . substr(file_get_contents("$repo_root/salt.txt"), 0, 32));
+}
+
+$settings['file_temp_path'] = '/tmp';
+
+// If this is changed, be sure to change it in the
+// factory-hooks/post-settings-php/includes.php file
+$settings['config_sync_directory'] = $repo_root . "/docroot/profiles/custom/stanford_profile/config/sync";
