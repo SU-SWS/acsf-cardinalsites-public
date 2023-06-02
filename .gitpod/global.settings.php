6c897d91349457b5a622ed8b07ab57bc88d8a866
pookmish <pookmish@stanford.edu>
2023-06-02 11:13:29 -0700

    Increased memory limit

--- .gitpod/global.settings.php
+++ .gitpod/global.settings.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 // Print errors on WSOD.
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
+ini_set('memory_limit', '256M');
 
 if (!getenv('STANFORD_ENCRYPT')) {
   putenv("STANFORD_ENCRYPT=" . substr(file_get_contents("$repo_root/salt.txt"), 0, 32));
