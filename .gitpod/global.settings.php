7a9a4df6972ba774c15eef12379f2ca5e3ca2e47
pookmish <pookmish@stanford.edu>
2023-10-04 10:48:52 -0500

    Update everything for Drupal 10 (#3)

--- .gitpod/global.settings.php
+++ .gitpod/global.settings.php
@@ -1,9 +1,6 @@
 <?php
 
 error_reporting(E_ALL & ~E_DEPRECATED);
-// Print errors on WSOD.
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
 
 if (PHP_SAPI !== 'cli') {
   ini_set('memory_limit', '256M');
@@ -18,8 +15,5 @@ $settings['file_temp_path'] = '/tmp';
 // If this is changed, be sure to change it in the
 // factory-hooks/post-settings-php/includes.php file
 $settings['config_sync_directory'] = $repo_root . "/docroot/profiles/custom/stanford_profile/config/sync";
-
-// Saml login doesn't work on gitpod. So disable it.
-$config['simplesamlphp_auth.settings']['activate'] = FALSE;
 
 $settings['container_yamls'][] = __DIR__ . '/../local.services.yml';
